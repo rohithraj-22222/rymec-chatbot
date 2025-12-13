79c998559d28b09aa52fef7bb09c47a3434f39a9
GAMEMUSC <151130580+rohithraj-22222@users.noreply.github.com>
2025-12-13 18:17:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,5 +61,9 @@ def chat():
         "year": year
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
